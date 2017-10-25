9112db913f19ff4d35a2ab74e67f106ce9bbd4ff
Adrian Wojtczak <xplolel@gmail.com>
2017-10-25 16:21:06 +0200

    Init
    Added mobile tasks and instruction

---
+++ app/src/main/java/akai/com/akaimobiletask/LoginActivity.java
@@ -0,0 +1,127 @@
+package akai.com.akaimobiletask;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    // UI references.
+    private EditText mEmailView;
+    private EditText mPasswordView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        // Set up the login form.
+        mEmailView = (EditText) findViewById(R.id.email);
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+
+    }
+
+    private void attemptLogin() {
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            mPasswordView.setError(getString(R.string.error_invalid_password));
+            focusView = mPasswordView;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmailView.setError(getString(R.string.error_invalid_email));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            moveOn();
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        return true; //TODO
+    }
+
+    private boolean isPasswordValid(String password) {
+        return true; //TODO
+    }
+
+    private void moveOn() {
+        Intent moveOnToMainActivityIntent = new Intent(this, MainActivity.class);
+
+        SharedPreferences settings = getSharedPreferences("login", Context.MODE_PRIVATE);
+
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("editable", "true");
+        editor.commit();
+
+        finish();
+
+        startActivity(moveOnToMainActivityIntent);
+    }
+
+    private void moveOnWithoutLoggingIn() { //TODO use this
+        Intent moveOnToMainActivityIntent = new Intent(this, MainActivity.class);
+
+        finish();
+
+        startActivity(moveOnToMainActivityIntent);
+    }
+
+}
+
---
+++ app/src/main/java/akai/com/akaimobiletask/MainActivity.java
@@ -0,0 +1,52 @@
+package akai.com.akaimobiletask;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button loginActivityButton;
+    private EditText textField;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        loginActivityButton = (Button) findViewById(R.id.mainButton);
+        loginActivityButton.setOnClickListener(new View.OnClickListener() {
+                                                   @Override
+                                                   public void onClick(View v) {
+                                                       goToLoginActivity();
+                                                   }
+                                               });
+
+        textField = (EditText) findViewById(R.id.mainText);
+
+        SharedPreferences settings = getSharedPreferences("login", Context.MODE_PRIVATE);
+        String myString = settings.getString("editable", "defaultvalue");
+        if(myString.equals("true"))
+        {
+            textField.setFocusable(true);
+            settings.edit().remove("editable").commit();
+        }
+
+    }
+
+    private void goToLoginActivity() {
+        Intent moveOnToMainActivityIntent = new Intent(this, LoginActivity.class);
+        onPause();
+        startActivity(moveOnToMainActivityIntent);
+        finish();
+    }
+}
